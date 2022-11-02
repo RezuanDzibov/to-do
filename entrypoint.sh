d74da7e86a8f66ab3220a3cb1ad8666a3a6e1561
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-02 21:57:14 +0300

    add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+  while ! nc -z db 5432; do
+    sleep 0.1
+  done
+
+bash -c "cd app && python manage.py flush --no-input"
+bash -c "cd app && python manage.py migrate"
+bash -c "cd app && python manage.py loaddata admin_fixture.json --app auth"
+exec "$@"
