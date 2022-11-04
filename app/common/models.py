3305abeafb4a634bfda4b6a8b64fc9a0b390a1a4
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-04 14:44:57 +0300

    add Image django model

--- app/common/models.py
+++ app/common/models.py
@@ -1,3 +1,18 @@
+import datetime
+from typing import Type
+
 from django.db import models
 
-# Create your models here.
+
+def get_upload_path(instance: Type[models.Model], filename: str):
+    """Construct image path"""
+    return f"{instance.__class__.__name__.lower()}/{'/'.join(str(datetime.date.today()).split('-'))}/{filename}"
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to=get_upload_path)
+
+    class Meta:
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
+
