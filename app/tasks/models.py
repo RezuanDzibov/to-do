723f2ad4ff945aa8e1142d86ae078ec9d89e9588
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-05 15:50:49 +0300

    add models to tasks app

---
+++ app/tasks/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+from common.models import Image
+from user.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Task Status"
+        verbose_name_plural = "Task Statuses"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TaskImage(Image):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    task = models.ForeignKey("Task", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Task Image"
+        verbose_name_plural = "Task Images"
+
+    def __str__(self) -> str:
+        return f"{self.task.id} task's {self.title}"
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self) -> str:
+        return self.title[:30]
+
+
+class TaskCompletion(models.Model):
+    task = models.OneToOneField(Task, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Task Completion"
+        verbose_name_plural = "Task Completions"
+
+    def __str__(self) -> str:
+        return f"{self.user.username} {self.task[:20]}"
