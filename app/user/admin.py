b3243198d5c03eb2cfaa33b9d613ea982c7331d1
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-06 21:01:45 +0300

    add UserAdmin customization

--- app/user/admin.py
+++ app/user/admin.py
@@ -6,4 +6,7 @@ from .models import User
 
 @register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("username", "email", "first_name", "last_name", "is_active")
+    list_filter = ("is_active", "is_staff", "is_superuser")
+    list_display_links = ("username", "email")
+
