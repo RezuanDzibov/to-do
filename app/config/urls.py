1d23ba49a215f963687a36c4ceec0bf9052ecc07
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-07 17:40:44 +0300

    move openapi staff to config/yasg.py

--- app/config/urls.py
+++ app/config/urls.py
@@ -15,31 +15,16 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
-
-from rest_framework import permissions
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
+from django.urls import path, include
 
 from app.config import settings
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="To Do API",
-        default_version="v1",
-        description="A To Do app",
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny]
-)
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
 ]
 
+urlpatterns += doc_urls
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
