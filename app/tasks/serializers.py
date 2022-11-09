03b9e4671bd9ff200a003a1f1e6fca7aff5c1fce
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-09 18:33:46 +0300

    add serializers.py in tasks app

---
+++ app/tasks/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from tasks import models
+
+
+class TaskCreateSerializerIn(serializers.ModelSerializer):
+    class Meta:
+        model = models.Task
+        fields = ["title", "category", "status", "text", "available"]
+
+
+class TaskCreateSerializerOut(serializers.ModelSerializer):
+    category = serializers.CharField(source="category.name")
+    status = serializers.CharField(source="status.name")
+
+    class Meta:
+        model = models.Task
+        fields = ["id", "category", "status", "user", "text", "available", "created_at"]
