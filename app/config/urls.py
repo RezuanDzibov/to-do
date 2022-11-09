84628cff16807dfd097db706519262a062db4d8e
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-09 18:36:14 +0300

    register task app urls to root urls.py

--- app/config/urls.py
+++ app/config/urls.py
@@ -17,13 +17,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from app.config import settings
+from . import settings
 from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
+    path("task/", include("tasks.urls"))
 ]
 
 urlpatterns += doc_urls
