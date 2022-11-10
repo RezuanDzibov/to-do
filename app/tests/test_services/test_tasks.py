c7c50972878f810ea01b36f8c2bd8503910ac261
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-10 19:06:40 +0300

    add TestGetTask to service tests

--- app/tests/test_services/test_tasks.py
+++ app/tests/test_services/test_tasks.py
@@ -1,5 +1,8 @@
+import random
+
 import pytest
 from django.contrib.auth import get_user_model
+from django.http import response
 from rest_framework import exceptions
 
 from tasks import models, services, serializers
@@ -7,7 +10,7 @@ from tasks import models, services, serializers
 User = get_user_model()
 
 
-class TestCreate:
+class TestTaskCreate:
     def test_success(self, admin_user: User, built_task: models.Task):
         task_serializer = serializers.TaskCreateSerializerIn(built_task)
         created_task = services.create_task(user=admin_user, data=task_serializer.data)
@@ -27,3 +30,13 @@ class TestCreate:
         with pytest.raises(exceptions.ValidationError) as exception:
             services.create_task(user=admin_user, data=task_serializer.data)
         assert exception.value.status_code == 400
+
+
+class TestGetTask:
+    def test_success(self, admin_user: User, task: models.Task):
+        task_in_db = services.get_task(id_=task.id)
+        assert task == task_in_db
+
+    def test_not_exist(self, db):
+        with pytest.raises(response.Http404):
+            services.get_task(id_=random.randint(1, 10))
