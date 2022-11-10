ec563512b40617dd13bd7c3130cef9b1173f6fd4
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-10 19:05:48 +0300

    register TaskRetrieve view in urls.py

--- app/tasks/urls.py
+++ app/tasks/urls.py
@@ -4,4 +4,5 @@ from tasks import views
 
 urlpatterns = [
     path("", views.CreateTask.as_view(), name="task-create"),
+    path("<int:task_id>/", views.TaskRetrieve.as_view(), name="task-retrieve")
 ]
