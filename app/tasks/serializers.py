ee83b189aad9ce171b16c63a962424312d491fbc
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-12 20:35:11 +0300

    add TaskListSerializer to tasks/serializers

--- app/tasks/serializers.py
+++ app/tasks/serializers.py
@@ -25,3 +25,13 @@ class TaskRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Task
         fields = ["id", "category", "status", "user", "text", "available", "created_at", "edited_at"]
+
+
+class TaskListSerializer(serializers.ModelSerializer):
+    category = serializers.CharField(source="category.name")
+    status = serializers.CharField(source="status.name")
+    user = serializers.CharField(source="user.username")
+
+    class Meta:
+        model = models.Task
+        fields = ["id", "category", "status", "user", "available"]
