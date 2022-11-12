b8d82cba145ff4aecdf121dcf4724c36676a5b79
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-12 20:36:24 +0300

    register TaskListview to tasks/urls and refactor task create url path

--- app/tasks/urls.py
+++ app/tasks/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
-    path("", views.CreateTask.as_view(), name="task-create"),
-    path("<int:task_id>/", views.TaskRetrieve.as_view(), name="task-retrieve")
+    path("list/", views.TaskList.as_view(), name="task-list"),
+    path("create/", views.CreateTask.as_view(), name="task-create"),
+    path("<int:task_id>/", views.TaskRetrieve.as_view(), name="task-retrieve"),
 ]
