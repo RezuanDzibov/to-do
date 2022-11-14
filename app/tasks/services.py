7c759e2d70ff6b91212507b8253cb126d6a03c2e
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-14 19:21:06 +0300

    add update_task service

--- app/tasks/services.py
+++ app/tasks/services.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import FieldDoesNotExist
 from django.shortcuts import get_object_or_404
+from rest_framework import exceptions
 
 from tasks import models
 from tasks import serializers
@@ -24,3 +26,18 @@ def get_task(id_: int) -> models.Task:
 def delete_task(user: User, id_: int) -> None:
     task = get_object_or_404(models.Task, id=id_, user=user)
     task.delete()
+
+
+def update_task(user: User, id_: int, data: dict) -> models.Task:
+    task = models.Task.objects.filter(id=id_)
+    if not task:
+        raise exceptions.NotFound()
+    if user.id != task[0].user.id:
+        raise exceptions.PermissionDenied()
+    try:
+        task.update(**data)
+    except FieldDoesNotExist as exception:
+        raise exceptions.ValidationError(detail=exception.args[0])
+    task = task[0]
+    return task
+
