279224113ff8772dacc4a36d1583348c0497b70d
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-14 19:23:10 +0300

    register TaskUpdate view to tasks/urls

--- app/tasks/urls.py
+++ app/tasks/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("list/", views.TaskList.as_view(), name="task-list"),
     path("create/", views.CreateTask.as_view(), name="task-create"),
     path("<int:task_id>/", views.TaskRetrieve.as_view(), name="task-retrieve"),
-    path("delete/<int:task_id>/", views.TaskDelete.as_view(), name="task-delete")
+    path("delete/<int:task_id>/", views.TaskDelete.as_view(), name="task-delete"),
+    path("update/<int:task_id>/", views.TaskUpdate.as_view(), name="task-update")
 ]
