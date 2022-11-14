4024ea3def5ba84e3d15c5a8e0bbf6e277fa9a07
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-14 19:23:58 +0300

    add TestTaskUpdate to services tests

--- app/tests/test_services/test_tasks.py
+++ app/tests/test_services/test_tasks.py
@@ -67,3 +67,30 @@ class TestDeleteTask:
     def test_not_exist_task_without_tasks(self, admin_user: User):
         with pytest.raises(Http404):
             services.delete_task(user=admin_user, id_=1)
+
+
+class TestTaskUpdate:
+    def test_success(self, admin_user: User, task: models.Task):
+        data_for_update = {"title": "another", "available": False}
+        task = services.update_task(user=admin_user, id_=task.id, data=data_for_update)
+        assert task.title == data_for_update["title"] and task.available == data_for_update["available"]
+
+    def test_with_multiple_tasks(self, admin_user: User, tasks: List[models.Task]):
+        data_for_update = {"title": "another", "available": False}
+        task = services.update_task(user=admin_user, id_=random.choice(tasks).id, data=data_for_update)
+        assert task.title == data_for_update["title"] and task.available == data_for_update["available"]
+
+    def test_invalid_data(self, admin_user: User, task: models.Task):
+        data_for_update = {"some": "another", "another_some": "some"}
+        with pytest.raises(exceptions.ValidationError):
+            services.update_task(user=admin_user, id_=task.id, data=data_for_update)
+
+    def test_not_author(self, user_and_its_password: User, task: models.Task):
+        data_for_update = {"title": "another", "available": False}
+        with pytest.raises(exceptions.PermissionDenied):
+            services.update_task(user=user_and_its_password["user"], id_=task.id, data=data_for_update)
+
+    def test_not_exist_task(self, admin_user: User):
+        data_for_update = {"title": "another", "available": False}
+        with pytest.raises(exceptions.NotFound):
+            services.update_task(user=admin_user, id_=random.randint(1, 100), data=data_for_update)
