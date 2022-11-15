97456bdb2f4b27e79bc957d2ece7064d89e561a8
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-15 18:51:30 +0300

    add CategorySerializer

--- app/tasks/serializers.py
+++ app/tasks/serializers.py
@@ -35,3 +35,9 @@ class TaskListSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Task
         fields = ["id", "category", "status", "user", "available"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = ["id", "name"]
