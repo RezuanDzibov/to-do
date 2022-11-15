1d433116585e51fd647a1b431fd0cca97d15ae99
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-11-15 18:52:01 +0300

    add IsStaffOrReadOnly permission

--- app/common/permissions.py
+++ app/common/permissions.py
@@ -5,3 +5,13 @@ from rest_framework import permissions
 class IsActive(permissions.BasePermission):
     def has_permission(self, request: HttpRequest, view) -> bool:
         return bool(request.user and request.user.is_active)
+
+
+class IsStaffOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated and
+            request.user.is_staff
+        )
