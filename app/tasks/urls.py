25a7c5c823119c34c3fb6a927e78ec1e9fbd155d
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-12-20 19:39:09 +0300

    register image creation and image retrieve urls

--- app/tasks/urls.py
+++ app/tasks/urls.py
@@ -12,7 +12,9 @@ urlpatterns = [
     path("create/", views.CreateTask.as_view(), name="task-create"),
     path("<int:task_id>/", views.TaskRetrieve.as_view(), name="task-retrieve"),
     path("delete/<int:task_id>/", views.TaskDelete.as_view(), name="task-delete"),
-    path("update/<int:task_id>/", views.TaskUpdate.as_view(), name="task-update")
+    path("update/<int:task_id>/", views.TaskUpdate.as_view(), name="task-update"),
+    path("image/create/", views.TaskImageCreate.as_view(), name="task_image-create"),
+    path("image/<int:pk>/", views.TaskImageRetrieve.as_view(), name="task_image-retrieve")
 ]
 
 urlpatterns += router.urls
