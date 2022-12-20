def199f87e0305f15555a3ade0d0d7b7a25a1271
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2022-12-20 19:41:02 +0300

    add TaskImageFactory

--- app/tests/factories.py
+++ app/tests/factories.py
@@ -1,5 +1,6 @@
 import factory
 from django.contrib.auth import get_user_model
+from django.core.files.base import ContentFile
 from django.utils.text import slugify
 from faker import Faker
 
@@ -43,3 +44,16 @@ class UserFactory(factory.django.DjangoModelFactory):
     is_staff = False
     is_active = True
     password = factory.LazyAttribute(lambda obj: fake.password())
+
+
+class TaskImageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.TaskImage
+    title = factory.LazyAttribute(lambda obj: fake.color_name())
+    image = factory.LazyAttribute(
+        lambda _: ContentFile(
+            factory.django.ImageField()._make_data(
+                {"width": 1024, "height": 768}
+            ), "example.jpg"
+        )
+    )
